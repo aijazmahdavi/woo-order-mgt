d960990a1268ad1f606403b2727b2a0ac34c4399
aijazmahdavi <aijazmahdavi@gmail.com>
2025-10-27 22:42:41 +0500

    Add env and variables in it, switched python to 3.12.11

--- woo_order_mgt/settings.py
+++ woo_order_mgt/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+from woo_order_mgt.env import *
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f())d40)o@#6#s6pg#^(qa_q%qo5$1zwi47q-w$d(1&47!kmb)'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = DEBUG
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [HOSTS]
 
 
 # Application definition
